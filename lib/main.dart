99a255c803e47c8e25e3e00e42ab4db14baadc80
Madushan_Joel <madushanjoel@gmail.com>
2023-08-08 08:56:36 +0000

    commit to master

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:helloworld/widgets/botts.dart';
 // Update the import statement
 import 'package:sensors_plus/sensors_plus.dart';
 
+//Master
 void main() {
   runApp(MaterialApp(
     home: MyApp(),
